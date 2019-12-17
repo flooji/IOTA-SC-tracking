87c15d0b6022e1907cf5ec37d7572633ad075ba6
flooji <florella240@gmail.com>
2019-12-17 12:16:25 +0100

    deployed app

--- src/App.vue
+++ src/App.vue
@@ -6,3 +6,4 @@ export default {
   name: 'app',
 }
 </script>
+
---
+++ src/views/404.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="hello">
+      <h1>(404) Page NotFound</h1>  
+    </div>
+</template>
+
+<script>
+export default {
+    name: '404'
+}
+</script>
